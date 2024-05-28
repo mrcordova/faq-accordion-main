423e96244470958e9ffd52e2009220f2761acbfb
mrcordova <46950394+mrcordova@users.noreply.github.com>
2024-05-27 19:00:10 -0700

    add logic for changing plus and minus img

--- js/script.js
+++ js/script.js
@@ -5,12 +5,17 @@ let showingPara = false;
 const showPara = (e) => {
     // console.log(e.currentTarget); 
     const para = e.currentTarget.querySelector('p');
+    const img = e.currentTarget.querySelector('img');
     if (para.classList.contains('hide')) {
         para.classList.remove('hide');
+        img.src = `${String(img.src).replace('plus', 'minus') }`
+        
+
     } else {
         para.classList.add('hide');
+        img.src = `${String(img.src).replace('minus', 'plus') }`
     }
-    console.log(para.classList);
+    console.log(img.src);
 }
 
 for (const qHeading of qCon) {
